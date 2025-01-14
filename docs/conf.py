e7dd4c3348ea2e790e291b09560d482a1871c80b
Miute <miute.dev@gmail.com>
2025-01-14 19:48:12 +0900

    Exclude pybind11 private functions from API documentation
    Exclude functions: `_pybind11_conduit_v1_()`

--- docs/conf.py
+++ docs/conf.py
@@ -86,5 +86,5 @@ autodoc_default_options = {
     "undoc-members": True,
     "special-members": True,
     "private-members": True,
-    "exclude-members": "__annotations__, __module__, __repr__",
+    "exclude-members": "__annotations__, __module__, __repr__, _pybind11_conduit_v1_",
 }
